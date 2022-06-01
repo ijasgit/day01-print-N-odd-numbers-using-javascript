11a9c5a78e0c6cb177d264e0127b34c4189f6568
Ijasgit <mohammedijasn3899@gmail.com>
2022-06-01 16:52:17 +0530

    n-odd-numbers.

---
+++ print-N-odd-numbers.js
@@ -0,0 +1,16 @@
+// let num,i;
+// i=1;
+// num=11;
+// while (i<=num) {
+//     console.log(+i);
+//     i=i+2;
+// }
+var num, i;
+num = 11;
+for (i = 0; i <= num; i++) {
+    while (i % 2 != 0) {
+        console.log(+i);
+        break;
+    }
+   
+}
